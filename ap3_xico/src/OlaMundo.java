eeb908b1d40a42803f435a321bdd73c421c42c77
ap3ufersa <ap3ufersa@gmail.com>
2023-07-21 20:45:04 -0300

    Adicionando exercicio 1.

--- ap3_xico/src/OlaMundo.java
+++ ap3_xico/src/OlaMundo.java
@@ -1,5 +1,5 @@
 public class OlaMundo {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Ola Mundo!");
     }
 }
