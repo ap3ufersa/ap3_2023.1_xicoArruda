f1e10f0069dcc2e698b881e2fe6074de73883eeb
ap3ufersa <ap3ufersa@gmail.com>
2023-08-11 19:02:14 -0300

    Banco e Produto

--- ap3_xico/src/br/com/xico/aula3/produto/TestaProduto.java
+++ ap3_xico/src/br/com/xico/aula3/produto/TestaProduto.java
@@ -4,10 +4,7 @@ package br.com.xico.aula3.produto;
 import java.util.ArrayList;
 
 public class TestaProduto {
-
+    // codigoSecreto = 1234
     public static void main(String[] args) {
-        Produto pipoca = new Produto("Bokus", 1.5, 4);
-        System.out.println(pipoca);
-
-    }
+        Produto pipoca = new Produto("
 }
