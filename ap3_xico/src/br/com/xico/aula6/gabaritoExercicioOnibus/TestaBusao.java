e8a4bb209e1f8c309eab2128865b1ff5d79f0b8c
ap3ufersa <ap3ufersa@gmail.com>
2023-08-25 14:43:05 -0300

    Ajustando teste

--- ap3_xico/src/br/com/xico/aula6/gabaritoExercicioOnibus/TestaBusao.java
+++ ap3_xico/src/br/com/xico/aula6/gabaritoExercicioOnibus/TestaBusao.java
@@ -6,21 +6,13 @@ public class TestaBusao {
 
   public static void main(String[] args) {
     // Criando um passageiro
-    Passageiro xico = new Passageiro(
-        "Xico",
-        "123456789",
-        LocalDate.of(2000, 8, 25));
+    Passageiro xico = new Passageiro("Xico", "123456789", LocalDate.of(2000, 8, 25));
 
     // Criando assentos
     Assento assentoXico = new Assento(1, TipoAssento.JANELA);
 
     // Criando uma viagem
-    Viagem natalAngicos = new Viagem(
-        1,
-        "Natal",
-        "Angicos",
-        "2023-08-25 08:00",
-        "2023-08-25 12:00");
+    Viagem natalAngicos = new Viagem(1, "Natal", "Angicos", "2023-08-25 08:00", "2023-08-25 12:00");
 
     // Listando todos os assentos antes da reserva
     System.out.println("\nTodos os assentos antes da reserva:");
