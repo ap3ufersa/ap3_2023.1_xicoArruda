c501fd8bf51b2719540598beb55e69cae08e21bf
AP3 - UFERSA Angicos <ap3ufersa@gmail.com>
2023-10-06 15:16:27 -0300

    Threads e Exceptions

---
+++ unidade3/threads/SequencialOuParalelo.java
@@ -0,0 +1,44 @@
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.IntStream;
+
+public class SequencialOuParalelo {
+
+  public static Duration paralelo(int quantNumeros) {
+    Instant inicio = Instant.now();
+    Map<Double, Double> mapa = new ConcurrentHashMap<>();
+    IntStream
+      .range(1, quantNumeros)
+      .parallel()
+      .mapToDouble(numero -> Math.pow(numero, 5))
+      .filter(numero -> numero % 2 == 0)
+      .forEach(numero -> mapa.put(numero, Math.pow(numero, 5)));
+    Instant fim = Instant.now();
+    return Duration.between(inicio, fim);
+  }
+
+  public static Duration sequencial(int quantNumeros) {
+    Instant inicio = Instant.now();
+    Map<Double, Double> mapa = new HashMap<>();
+    IntStream
+      .range(1, quantNumeros)
+      .mapToDouble(numero -> Math.pow(numero, 5))
+      .filter(numero -> numero % 2 == 0)
+      .forEach(numero -> mapa.put(numero, Math.pow(numero, 5)));
+    Instant fim = Instant.now();
+    return Duration.between(inicio, fim);
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Saida Formato ISO8601");
+	int quantNumeros = 3000000;
+	System.out.println("Duracao - Sequencial="+sequencial(quantNumeros));
+	System.out.println("Duracao - Paralelo="+paralelo(quantNumeros));
+    
+  }
+}
